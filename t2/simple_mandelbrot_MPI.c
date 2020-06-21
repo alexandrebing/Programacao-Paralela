8eaab223e950c65c20be86e631954489c102ff6e
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-06-20 21:17:18 -0300

    Mais uma tentativa

--- t2/simple_mandelbrot_MPI.c
+++ t2/simple_mandelbrot_MPI.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <mpi.h>
+#include <mpi.h> 
 
-#define NPOINTS 2000
-#define MAXITER 2000
+# define NPOINTS 2000
+# define MAXITER 2000
 
-struct complex
-{
-    double real;
-    double imag;
+
+struct complex{
+  double real;
+  double imag;
 };
 
-int main(int argc, char *argv[])
-{
-    int id, p, hs;
-    char h[MPI_MAX_PROCESSOR_NAME];
-    int i, j, iter, numoutside = 0;
-    double area, error, ztemp;
-    double start, finish;
-    struct complex z, c;
+int main(int argc, char *argv[]){
+  int i, j, iter, numoutside = 0;
+  double area, error, ztemp;
+  double start, finish;
+  struct complex z, c;
 
-    /*
+  /*
    *   
    *
    *     Outer loops run over npoints, initialise z=c
@@ -29,49 +26,47 @@ int main(int argc, char *argv[])
    *     Inner loop has the iteration z=z*z+c, and threshold test
    */
 
-    MPI_Init(&argc, &argv);
-    // MPI_Get_processor_name(h, &hs);
-    // MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    // MPI_Comm_size(MPI_COMM_WORLD, &p);
-    start = MPI_Wtime();
+  MPI_Init(&argc,&argv);
+//   MPI_Get_processor_name(h, &hs);
+//   MPI_Comm_rank(MPI_COMM_WORLD, &id);
+//   MPI_Comm_size(MPI_COMM_WORLD, &p);
+  start = MPI_Wtime();  
 
-    for (NPOINTS = 500; NPOINTS <= 5000; NPOINTS += 500)
-    {
-
-        for (i = 0; i < NPOINTS; i++)
-        {
-            for (j = 0; j < NPOINTS; j++)
-            {
-                c.real = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + 1.0e-7;
-                c.imag = 1.125 * (double)(j) / (double)(NPOINTS) + 1.0e-7;
-                z = c;
-                for (iter = 0; iter < MAXITER; iter++)
-                {
-                    ztemp = (z.real * z.real) - (z.imag * z.imag) + c.real;
-                    z.imag = z.real * z.imag * 2 + c.imag;
-                    z.real = ztemp;
-                    if ((z.real * z.real + z.imag * z.imag) > 4.0e0)
-                    {
-                        numoutside++;
-                        break;
-                    }
-                }
-            }
+  for (NPOINTS = 500; NPOINTS <= 5000; NPOINTS += 500)
+{
+    printf(NPOINTS);
+}
+  
+  for (i=0; i<NPOINTS; i++) {
+    for (j=0; j<NPOINTS; j++) {
+      c.real = -2.0+2.5*(double)(i)/(double)(NPOINTS)+1.0e-7;
+      c.imag = 1.125*(double)(j)/(double)(NPOINTS)+1.0e-7;
+      z=c;
+      for (iter=0; iter<MAXITER; iter++){
+        ztemp=(z.real*z.real)-(z.imag*z.imag)+c.real;
+        z.imag=z.real*z.imag*2+c.imag; 
+        z.real=ztemp; 
+        if ((z.real*z.real+z.imag*z.imag)>4.0e0) {
+          numoutside++; 
+          break;
         }
+      }
     }
+  }
 
-    finish = MPI_Wtime();
 
-    /*
+  finish = MPI_Wtime();  
+
+  /*
    *  Calculate area and error and output the results
    */
 
-    area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
-    error = area / (double)NPOINTS;
+  area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
+  error=area/(double)NPOINTS;
 
-    printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n", area, error);
-    printf("Time = %12.8f seconds\n", finish - start);
-
-    MPI_Finalize();
-    return 0;
+  printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
+  printf("Time = %12.8f seconds\n",finish-start);
+   
+  MPI_Finalize();
+  return 0;
 }
