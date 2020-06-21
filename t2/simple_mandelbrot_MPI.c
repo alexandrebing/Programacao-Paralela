310ffbbbf0545c35176f52898557cd86848ce3b5
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-06-20 23:20:26 -0300

    testando versão paralela MPI

--- t2/simple_mandelbrot_MPI.c
+++ t2/simple_mandelbrot_MPI.c
@@ -13,7 +13,6 @@ struct complex
 
 int main(int argc, char *argv[])
 {
-
     int NPOINTS = 500;
     int i, j, iter;
     double area, error, ztemp;
@@ -28,45 +27,58 @@ int main(int argc, char *argv[])
    *     Inner loop has the iteration z=z*z+c, and threshold test
    */
 
-    MPI_Init(&argc, &argv);
-
     for (NPOINTS = 500; NPOINTS <= 2000; NPOINTS += 500)
     {
+        int i, id, p, total, sol, hs;
+        char h[MPI_MAX_PROCESSOR_NAME];
         start = MPI_Wtime();
+        int id = NPOINTS;
+        MPI_Init(&argc, &argv);
+        MPI_Get_processor_name(h, &hs);
+        MPI_Comm_rank(MPI_COMM_WORLD, &id);
+        MPI_Comm_size(MPI_COMM_WORLD, &p);
+        int sum = 0
         int numoutside = 0;
         for (i = 0; i < NPOINTS; i++)
         {
-            for (j = 0; j < NPOINTS; j++)
+            if (i % p == id)
             {
-                c.real = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + 1.0e-7;
-                c.imag = 1.125 * (double)(j) / (double)(NPOINTS) + 1.0e-7;
-                z = c;
-                for (iter = 0; iter < MAXITER; iter++)
+                for (j = 0; j < NPOINTS; j++)
                 {
-                    ztemp = (z.real * z.real) - (z.imag * z.imag) + c.real;
-                    z.imag = z.real * z.imag * 2 + c.imag;
-                    z.real = ztemp;
-                    if ((z.real * z.real + z.imag * z.imag) > 4.0e0)
+                    c.real = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + 1.0e-7;
+                    c.imag = 1.125 * (double)(j) / (double)(NPOINTS) + 1.0e-7;
+                    z = c;
+                    for (iter = 0; iter < MAXITER; iter++)
                     {
-                        numoutside++;
-                        break;
+                        ztemp = (z.real * z.real) - (z.imag * z.imag) + c.real;
+                        z.imag = z.real * z.imag * 2 + c.imag;
+                        z.real = ztemp;
+                        if ((z.real * z.real + z.imag * z.imag) > 4.0e0)
+                        {
+                            numoutside++;
+                            fflush(stdout);
+                            break;
+                        }
                     }
                 }
+                fflush(stdout);
             }
         }
+        MPI_Reduce(&numoutside, &sum, 1, MPI_INT,
+             MPI_SUM, 0, MPI_COMM_WORLD);
         finish = MPI_Wtime();
 
         /*
    *  Calculate area and error and output the results
    */
 
-        area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
+        area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - sum) / (double)(NPOINTS * NPOINTS);
         error = area / (double)NPOINTS;
 
         printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n", area, error);
         printf("Time = %12.8f seconds\n", finish - start);
+        MPI_Finalize();
     }
 
-    MPI_Finalize();
     return 0;
 }
